c7bad3ad71ebcf97f3b1cc4a885dd7391b4d1367
Kai Kunze <kaikunze@me.com>
2025-09-20 17:56:10 +0900

    fix css paths

---
+++ _app/immutable/chunks/C8PncKKO.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_b02uyb?.base??"/ahs",a=globalThis.__sveltekit_b02uyb?.assets??"https://kaikunze.de/ahs";export{a,s as b};
---
+++ _app/immutable/entry/start.COdxPva9.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/kt-gtpyk.js";import{x as t}from"../chunks/Dbrtc075.js";export{t as load_css,r as start};
