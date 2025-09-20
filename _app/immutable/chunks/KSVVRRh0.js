1815a299019eaedb5ab8765ae25f0660e34c716d
Kai Kunze <kaikunze@me.com>
2025-09-20 18:09:52 +0900

    testing

---
+++ _app/immutable/chunks/KSVVRRh0.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1l5t80k?.base??"/ahs",t=globalThis.__sveltekit_1l5t80k?.assets??s;export{t as a,s as b};
---
+++ _app/immutable/entry/start.BlazM0O9.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/D6i28tXn.js";import{x as t}from"../chunks/Cfxyaqz8.js";export{t as load_css,r as start};
