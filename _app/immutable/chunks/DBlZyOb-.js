a51ec50e30bf05a8d1e26bd71d6f04173bed827a
Kai Kunze <kaikunze@me.com>
2025-09-20 18:01:35 +0900

    fix?

---
+++ _app/immutable/chunks/DBlZyOb-.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_wvlyou?.base??"/ahs",a=globalThis.__sveltekit_wvlyou?.assets??s;export{a,s as b};
---
+++ _app/immutable/entry/start.kYWUWo3l.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CCuWuh9x.js";import{x as t}from"../chunks/BY502cxH.js";export{t as load_css,r as start};
